90e26f0f9fa8da0f18f0c3312f196ef5e83d6981
MuhammadSalah99 <mohmmedjunide73@gmail.com>
2023-05-04 16:47:31 +0300

    revisitng recursion

--- recursion.go
+++ recursion.go
@@ -5,6 +5,34 @@ package main
 import "fmt"
 
 
+func sum(num int) int {
+    if num == 1 {
+        return 1
+    }  else {
+        return num + sum(num-1)
+    }
+}
+
+func seperateDigits(num int) int {
+
+    if num == 0 {
+        return 0
+    }
+    digit := num % 10
+   return  digit + seperateDigits(num/10)
+
+}
+func isDivisibleBy3(num int)  bool {
+
+    if seperateDigits(num)%3 != 0 {
+        return false
+    }
+    return true
+
+}
+
 func main() {
     fmt.Println("hi from new file")
+    fmt.Println(sum(5))
+    fmt.Println(isDivisibleBy3(63))
 }
